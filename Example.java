3f050e372728c23761f1068637277585eed98396
Dennis Bijlsma <dennis.bijlsma@softwareimprovementgroup.com>
2025-12-05 09:05:31 +0100

    Initial commit.

---
+++ Example.java
@@ -0,0 +1,306 @@
+public class Example {
+
+    public void veryLongAndComplexMethod(int a, int b, int c, int d) {
+        if (a == b) {
+            if (b == c) {
+                if (c == d) {
+                    System.out.println("All equal");
+                } else {
+                    System.out.println("Not all equal");
+                }
+            } else {
+                System.out.println("Not all equal");
+            }
+        } else {
+            if (b == c) {
+                if (c == d) {
+                    System.out.println("C and D equal");
+                } else {
+                    System.out.println("Not equal");
+                }
+            } else {
+                System.out.println("Not equal");
+            }
+        }
+        
+        Example2 aap = new Example2();
+        aap.complexMethod2();
+        aap.complexMethod3();
+        aap.complexMethod4();
+        aap.complexMethod5();
+        aap.complexMethod6();
+        aap.complexMethod7();
+        aap.complexMethod8();
+        aap.complexMethod9();
+        aap.complexMethod10();
+        aap.complexMethod11();
+        aap.complexMethod12();
+        aap.complexMethod13();
+    }
+    
+    public int complexMethod(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod2(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod3(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod4(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod5(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod6(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod7(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod8(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            default : throw new AssertionError(e);
+        } 
+    }
+        
+    public int complexMethod9(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod10(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod11(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod13(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod14(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod15(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod16(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+}
---
+++ Example2.java
@@ -0,0 +1,304 @@
+public class Example2 {
+
+    public void veryLongAndComplexMethod(int a, int b, int c, int d) {
+        if (a == b) {
+            if (b == c) {
+                if (c == d) {
+                    System.out.println("All equal");
+                } else {
+                    System.out.println("Not all equal");
+                }
+            } else {
+                System.out.println("Not all equal");
+            }
+        } else {
+            if (b == c) {
+                if (c == d) {
+                    System.out.println("C and D equal");
+                } else {
+                    System.out.println("Not equal");
+                }
+            } else {
+                System.out.println("Not equal");
+            }
+        }
+    }
+    
+    public int complexMethod(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod2(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod3(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod4(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod5(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod6(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod7(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod8(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+        
+    public int complexMethod9(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    } 
+    
+    public int complexMethod10(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod11(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod13(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod14(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod15(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod16(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+}
