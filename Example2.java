eedfe386f6ce18c39d848a5a315f88a0a51e910b
Dennis Bijlsma <d.bijlsma@sig.eu>
2025-12-16 08:05:24 +0100

    Update example.

--- Example2.java
+++ Example2.java
@@ -113,25 +113,6 @@ public class Example2 {
         } 
     } 
     
-    public int complexMethod6(int a) {
-        switch (a) {
-            case 1 : return 1;
-            case 2 : return 2;
-            case 3 : return 3;
-            case 4 : return 4;
-            case 5 : return 5;
-            case 6 : return 6;
-            case 7 : return 7;
-            case 8 : return 8;
-            case 9 : return 9;
-            case 10 : return 10;
-            case 11 : return 11;
-            case 12 : return 12;
-            case 13 : return 13;
-            default : throw new AssertionError(e);
-        } 
-    } 
-    
     public int complexMethod7(int a) {
         switch (a) {
             case 1 : return 1;
@@ -303,6 +284,63 @@ public class Example2 {
     }
 
     public int complexMethod17(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+
+    public int complexMethod18(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+
+    public int complexMethod19(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            case 12 : return 12;
+            case 13 : return 13;
+            default : throw new AssertionError(e);
+        } 
+    }
+
+    public int complexMethod20(int a) {
         switch (a) {
             case 1 : return 1;
             case 2 : return 2;
